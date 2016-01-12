a5356eeef659de396c95a747dd077049ce11dff1
Laurent Meyer <laurent@moid.de>
2016-01-12 22:02:23 +0100

    Tested the app picking process partially

---
+++ sentinel/src/androidTest/java/de/tum/in/i22/sentinel/android/app/AppPickingProcessTest.java
@@ -0,0 +1,53 @@
+package de.tum.in.i22.sentinel.android.app;
+
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import de.tum.in.i22.sentinel.android.app.package_getter.PackageGetter;
+
+/**
+ * Created by Moderbord on 2016-01-12.
+ */
+
+public class AppPickingProcessTest extends AndroidTestCase {
+
+    int numberOfPackages;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        String command = "pm list packages -f";
+        Process process;
+        try {
+            process = Runtime.getRuntime().exec(command);
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+            String cLine;
+            while ((cLine = bufferedReader.readLine()) != null) {
+                if (cLine.contains("package"))
+                    numberOfPackages++;
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void testNumberOfPackages() throws Exception {
+        PackageGetter.getPackages(new PackageGetter.Callback() {
+            @Override
+            public void onError(Exception e) {
+                assertTrue("The getter returned an error", false);
+            }
+
+            @Override
+            public void onSuccess(List<PackageGetter.Package> packages) {
+                assertEquals("Number of packages is not consistent", numberOfPackages, packages.size());
+            }
+        });
+
+    }
+}
--- sentinel/src/main/java/de/tum/in/i22/sentinel/android/app/package_getter/PackageGetter.java
+++ sentinel/src/main/java/de/tum/in/i22/sentinel/android/app/package_getter/PackageGetter.java
@@ -179,7 +179,7 @@ public class PackageGetter {
         "package" is always there, then it's the path of the app to be, potentially, extracted.
         then comes the package name.
          */
-        return "(package:)([a-zA-Z/.\\-0-9]*)(=)([A-Za-z.]*)";
+        return "(package:)([a-zA-Z\\/.\\-0-9]*)(=)([A-Za-z\\.\\d\\_]*)";
     }
 
 
