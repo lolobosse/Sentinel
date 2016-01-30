9a0afd8491a5cc44ae106e390256fee027384184
Laurent Meyer <laurent@moid.de>
2016-01-30 16:02:58 +0100

    Test to ensure that the policy are properly copied on the device at the start of the app

--- sentinel/src/androidTest/java/de/tum/in/i22/sentinel/android/app/TransferTest.java
+++ sentinel/src/androidTest/java/de/tum/in/i22/sentinel/android/app/TransferTest.java
@@ -1,7 +1,26 @@
 package de.tum.in.i22.sentinel.android.app;
 
+import android.test.AndroidTestCase;
+
+import java.io.File;
+
 /**
  * Created by laurentmeyer on 30/01/16.
  */
-public class TransferTest {
+public class TransferTest extends AndroidTestCase {
+
+    public void testArePoliciesThere() throws Exception {
+        Utils.passPoliciesFromRawToFile(getContext());
+        String[] array = {"Policy.xml", "Policy_App_SMS_1.xml", "Policy_App_SMS_Duration_2.xml", "Policy_App_SMS_Duration_4.xml",
+                "Policy_Draft.xml", "Policy_DroidBench.xml", "Policy_No_Imei_to_12345.xml", "Policy_No_Location.xml", "Policy_Old.xml"};
+        for (String policy : array){
+            checkFilePresence(policy);
+        }
+    }
+
+    private void checkFilePresence(String s){
+        File sdcard = new File("/sdcard/SentinelPolicies/");
+        File toCheck = new File(sdcard, s);
+        assertTrue(toCheck.exists() && !toCheck.isDirectory());
+    }
 }
